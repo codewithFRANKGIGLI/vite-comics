56e184c5cc31c92ad97951bc76954d1a9c25939c
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-15 09:44:52 +0200

    feat: vbase comp

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+    <script>
+      import { AppHeader } from "AppHeader.vue";
+      import { AppMainContent } from "AppMainContent.vue";
+      import { AppFeatures } from "AppFeatures.vue";
+      import { AppTopFooter } from "AppTopFooter.vue";
+      import { AppBottomFooter } from "AppBottomFooter.vue";
+      export default {
+        AppHeader,
+        AppMainContent,
+        AppFeatures,
+        AppTopFooter,
+        AppBottomFooter,
+      }
+    </script>
+
+<template>
+  <div>
+    <AppHeader></AppHeader>
+    <AppMainContent></AppMainContent>
+    <AppFeatures></AppFeatures>
+    <AppTopFooter></AppTopFooter>
+    <AppBottomFooter></AppBottomFooter>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
